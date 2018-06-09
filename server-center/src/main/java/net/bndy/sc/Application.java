230cdb8b0564900251329fa7e37e59cbb4de2633
Bendy Zhang <zb@bndy.net>
2018-06-09 09:35:26 +0800

    add banner

--- server-center/src/main/java/net/bndy/sc/Application.java
+++ server-center/src/main/java/net/bndy/sc/Application.java
@@ -2,8 +2,6 @@ package net.bndy.sc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaServer
 @RestController
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
 	}
 	
     @RequestMapping("/hi")
