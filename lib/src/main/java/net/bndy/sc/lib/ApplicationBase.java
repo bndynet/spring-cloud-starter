279bd0df2943ee368b68488c7465fedf319d1750
Bendy Zhang <zb@bndy.net>
2018-06-10 21:42:37 +0800

    support war package

--- lib/src/main/java/net/bndy/sc/lib/ApplicationBase.java
+++ lib/src/main/java/net/bndy/sc/lib/ApplicationBase.java
@@ -1,17 +1,31 @@
+/**
+ * Copyright (c) 2018 BNDY-NET. All Rights Reserved.
+ * http://bndy.net
+ */
 package net.bndy.sc.lib;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ * @author Bendy Zhang
+ * @version 1.0
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public abstract class ApplicationBase {
+public abstract class ApplicationBase extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+	
     @Value("${spring.application.name}")
     String name;
 
--- lib/src/main/java/net/bndy/sc/lib/SecurityConfig.java
+++ lib/src/main/java/net/bndy/sc/lib/SecurityConfig.java
@@ -1,9 +1,17 @@
+/**
+ * Copyright (c) 2018 BNDY-NET. All Rights Reserved.
+ * http://bndy.net
+ */
 package net.bndy.sc.lib;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * @author Bendy Zhang
+ * @version 1.0
+ */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
