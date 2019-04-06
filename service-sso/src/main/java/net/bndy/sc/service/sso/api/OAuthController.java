700aafe009ad99ac51848a56a229d935c0677358
Bendy Zhang <zb@bndy.net>
2019-04-06 19:31:49 +0800

    feat(sso): api for oauth logout

--- service-sso/src/main/java/net/bndy/sc/service/sso/api/OAuthController.java
+++ service-sso/src/main/java/net/bndy/sc/service/sso/api/OAuthController.java
@@ -4,12 +4,17 @@
  */
 package net.bndy.sc.service.sso.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.bndy.sc.service.sso.entity.AppUser;
- 
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Bendy Zhang
  * @version 1.0
@@ -19,6 +24,18 @@ public class OAuthController {
 
     @GetMapping("/oauth/me")
     public AppUser user(Authentication principal) {
-        return (AppUser)principal.getPrincipal();
+        return (AppUser) principal.getPrincipal();
+    }
+
+    @Autowired
+    ConsumerTokenServices tokenServices;
+
+    @DeleteMapping("/oauth/logout")
+    public void revokeToken(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+        if (authorization != null && authorization.contains("Bearer")) {
+            String tokenId = authorization.substring("Bearer".length() + 1);
+            tokenServices.revokeToken(tokenId);
+        }
     }
 }
