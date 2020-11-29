48b2fd7421f1bb1bb4ad7a2d855b6581f06b0fd6
Bendy Zhang <zb@bndy.net>
2020-11-29 17:22:30 +0800

    feat(res): add stacktrace to error log

--- service-resource/src/main/java/net/bndy/res/core/ApiError.java
+++ service-resource/src/main/java/net/bndy/res/core/ApiError.java
@@ -20,19 +20,19 @@ public class ApiError {
         timestamp = LocalDateTime.now();
     }
 
-    ApiError(HttpStatus status) {
+    public ApiError(HttpStatus status) {
         this();
         this.status = status;
     }
 
-    ApiError(HttpStatus status, Throwable ex) {
+    public ApiError(HttpStatus status, Throwable ex) {
         this();
         this.status = status;
         this.message = "Unexpected error";
         this.debugMessage = ex.getLocalizedMessage();
     }
 
-    ApiError(HttpStatus status, String message, Throwable ex) {
+    public ApiError(HttpStatus status, String message, Throwable ex) {
         this();
         this.status = status;
         this.message = message;
--- service-resource/src/main/java/net/bndy/res/core/ApiExceptionHandler.java
+++ service-resource/src/main/java/net/bndy/res/core/ApiExceptionHandler.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
-
 import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
@@ -17,7 +15,10 @@ public class ApiExceptionHandler {
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError defaultExceptionHandler(Exception ex, HttpServletRequest request) {
 
-        this.logger.error("{} - " + ex.getMessage(), request.getRequestURI());
+        this.logger.error("{} - {} \n{}",
+                request.getRequestURI(),
+                ex.getMessage(),
+                ex.getStackTrace());
 
         ApiError error = new ApiError(
                 HttpStatus.INTERNAL_SERVER_ERROR,
